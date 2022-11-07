63e545a6547b44dad33c97cdbec2144be0c85e4e
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2022-11-07 12:41:11 +0000

    "sign up done"

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,67 +8,48 @@ const User = require("../models/User.model");
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
-router.get("/signup", isLoggedOut, (req, res) => {
+router.get("/signup", isLoggedOut, (req, res, next) => {
   res.render("auth/signup");
 });
 
-router.post("/signup", isLoggedOut, (req, res) => {
-  const { username, email, password } = req.body;
+router.post('/signup', async (req, res, next) => {
+  const { firstName, lastName, countryOfBirth, residence, email, password } = req.body;
 
-  if (!username || !email || !password) {
-    res.status(400).render("auth/signup", {
-      errorMessage:
-        "All fields are mandatory. Please provide your username, email and password.",
-    });
+  try {
+    if (!firstName || !lastName ||!countryOfBirth || !residence || !email || !password) {
+      res.render('auth/signup', {
+        errorMessage: 'All the fields are mandatory. Please input a username, email and passowrd',
+      });
+      return;
+    }
 
-    return;
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!regex.test(password)) {
+      res.status(500).render('auth/signup', {
+        errorMessage:
+          'Invalid password, password needs to have at least 6 characters and include an uppercase and lowercase character',
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const createdUser = await User.create({ firstName, lastName, countryOfBirth, residence, email, password: hashedPassword });
+
+    res.redirect('/');
+  } catch (error) {
+    console.log(error);
+    if (error instanceof mongoose.Error.ValidationError) {
+      res.status(500).render('auth/signup', { errorMessage: error.message });
+    } else if (error.code === 11000) {
+      res.status(500).render('auth/signup', { errorMessage: ' Username or email already exists' });
+    }
+
+    next(error);
   }
-
-  if (password.length < 6) {
-    res.status(400).render("auth/signup", {
-      errorMessage: "Your password needs to be at least 6 characters long.",
-    });
-
-    return;
-  }
-
-  //   ! This regular expression checks password for special characters and minimum length
-  /*
-  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-  if (!regex.test(password)) {
-    res
-      .status(400)
-      .render("auth/signup", {
-        errorMessage: "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter."
-    });
-    return;
-  }
-  */
-
-  // Create a new user - start by hashing the password
-  bcrypt
-    .genSalt(saltRounds)
-    .then((salt) => bcrypt.hash(password, salt))
-    .then((hashedPassword) => {
-      // Create a user and save it in the database
-      return User.create({ username, email, password: hashedPassword });
-    })
-    .then((user) => {
-      res.redirect("/auth/login");
-    })
-    .catch((error) => {
-      if (error instanceof mongoose.Error.ValidationError) {
-        res.status(500).render("auth/signup", { errorMessage: error.message });
-      } else if (error.code === 11000) {
-        res.status(500).render("auth/signup", {
-          errorMessage:
-            "Username and email need to be unique. Provide a valid username or email.",
-        });
-      } else {
-        next(error);
-      }
-    });
 });
+
 
 /* _____________________________________ LOGIN _____________________________________________ */
 
@@ -135,3 +116,6 @@ router.post("/login", isLoggedOut, (req, res, next) => {
 });
  */
 module.exports = router;
+
+
+
